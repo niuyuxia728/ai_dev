88f16e3844caf3cb6628d1d6ed023efe1f46f1e8
niuyuxia728 <147525479+niuyuxia728@users.noreply.github.com>
2025-12-14 20:52:02 +0000

    migrate folders

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# ai_dev
+# part 1
+## using AI tools to develop website based on Django_template
+## minimum code developing
--- todo/myapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import todo_list, delete_todo, complete_todo
-
-urlpatterns = [
-    path('', todo_list, name='todo_list'),
-    path('delete/<int:todo_id>/', delete_todo, name='delete_todo'),
-    path('complete/<int:todo_id>/', complete_todo, name='complete_todo'),
-]
